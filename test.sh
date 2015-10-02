9d9fc6b5be64f985e05e8e127b71d0e10355e154
romeOz <serggalka@gmail.com>
2015-10-02 14:31:06 +0300

    Fixed test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ echo "-- Testing memcached is running"
 
 docker run --name base_1 -d memcahed-test; sleep 10
 docker run --name base_2 -d --link base_1:base_1 memcahed-test; sleep 10
-docker exec -it base_2 bash -c 'apt-get update && apt-get install -y telnet'; sleep 5
+docker exec -it base_2 bash -c 'apt-get update >/dev/null && apt-get install -y telnet >/dev/null'; sleep 5
 docker exec -it base_2 bash -c '( echo "version";  sleep 1;  echo "quit" ) | telnet base_1 11211' | grep -wc "VERSION"
 
 echo ""
