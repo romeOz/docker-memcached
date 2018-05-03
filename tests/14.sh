6ffd8bdb4790e132af7194fdb1dd00fe4ef61921
romeOz <serggalka@gmail.com>
2018-05-03 04:42:28 +0300

    Change structure directories

--- tests/14.sh
+++ tests/14.sh
@@ -3,15 +3,15 @@ set -e
 
 echo
 echo
-echo "-- Building Memcahed image"
-docker build -t memcahed-test .
+echo "-- Building Memcahed 1.4 image"
+docker build -t memcahed-14 ../1.4/
 docker network create mem_net
 
 echo
 echo "-- Testing memcached is running"
 
-docker run --name base_1 -d --net mem_net memcahed-test; sleep 10
-docker run --name base_2 -d --net mem_net memcahed-test; sleep 10
+docker run --name base_1 -d --net mem_net memcahed-14; sleep 10
+docker run --name base_2 -d --net mem_net memcahed-14; sleep 10
 docker exec -it base_2 bash -c 'apt-get update >/dev/null && apt-get install -y telnet >/dev/null'; sleep 5
 docker exec -it base_2 bash -c '( echo "version";  sleep 1;  echo "quit" ) | telnet base_1 11211' | grep -wc "VERSION"
 
@@ -19,4 +19,4 @@ echo
 echo "-- Clear"
 docker rm -f -v base_1 base_2; sleep 5
 docker network rm mem_net
-docker rmi -f memcahed-test
+docker rmi -f memcahed-14
